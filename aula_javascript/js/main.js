929f4696b93c1accba03c465ed8ecd915a4f0b0c
velasco-willer <84729838+velasco-willer@users.noreply.github.com>
2021-05-25 23:19:27 -0300

    Add files via upload

---
+++ aula_javascript/js/main.js
@@ -0,0 +1,111 @@
+function botao(){
+	alert("obrigado por clicar");
+}
+
+function redirecionar(){
+	window.open("https://globallabs.academy/");
+	//window.location.href = "https://globallabs.academy/";
+}
+
+function trocar(){
+	alert("trocar texto");
+}
+
+/*
+function soma(n1, n2)
+{
+	return n1+n2;
+}
+//alert (soma(5,10));
+
+function validaIdade(idade)
+{
+	if(idade >= 18)
+		validar=true;
+	else
+		var validar=false;
+	return validar;
+}
+var idade =prompt("Qual sua idade");
+
+console.log(validaIdade(idade));
+
+
+
+
+/*
+var d =new Date();
+alert (d);
+alert (d.getDay());
+alert (d.getHours());
+alert (d.getMinutes());
+
+
+
+var count;
+
+for(count=0; count<5; count++)
+{
+	alert(count);
+}
+
+
+
+
+
+var count = 0;
+
+while (count < 5){
+	console.log(count);
+	alert(count);
+	count = count+1;
+	
+};
+
+
+
+
+
+
+
+var idade = prompt("Qual sua idade");
+
+if(idade >= 18)
+{
+	alert("maior de idade");
+}
+else
+{
+	alert("menor de idade");
+}
+
+
+
+var fruta = [{nome:"maca", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(fruta);
+alert(fruta[1].cor);
+
+
+var frutas = {nome:"maca", cor:"vermelha"}
+console.log(frutas.nome);
+alert(frutas.cor);
+
+
+var lista = ["maça", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse);
+alert(lista[1]);
+
+
+
+var nome = "Willer Velasco";
+var idade = 22;
+var idade2 =10;
+//alert (nome + " tem " + idade + " anos");
+//alert (idade + idade2);
+
+console.log(nome);
+console.log(idade2);*/
